6bf5bc47091838e73f6bc2df5b77f07cd60864d0
cellist <cellist@squizzy.de>
2020-10-02 18:11:10 +0200

    Added reference to web tutorial, giving sources more convenient names

--- cmake-tut/hello-world-example/build_and_run.sh
+++ cmake-tut/hello-world-example/build_and_run.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+# Borrowed heavily from https://riptutorial.com/cmake/example/7501/simple--hello-world--project
+#
 mkdir -p build
 cd build && cmake .. && cmake --build .
-../build/app
+../build/hello-world
