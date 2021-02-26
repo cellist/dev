348fd6d462fa31c8311d190615b51959cafa36ec
cellist <cellist@squizzy.de>
2021-02-26 10:25:37 +0100

    Code coverage analysis added.

--- qt/LoopBackExample/go.sh
+++ qt/LoopBackExample/go.sh
@@ -5,6 +5,10 @@ etags *.cpp
 mkdir -p build &&\
     cd build &&\
     env CXX=/usr/bin/clang++ cmake .. &&\
-    cmake  --build .
+    cmake -DCODE_COVERAGE=ON --build . &&\
+    make &&\
+    cd .. &&\
+    env LLVM_PROFILE_FILE="./build/my.profraw" ./build/loopback-example &&\
+    llvm-profdata merge -sparse ./build/my.profraw -o ./build/my.profdata &&\
+    llvm-cov show ./build/loopback-example -instr-profile=./build/my.profdata
 
-cd ..
