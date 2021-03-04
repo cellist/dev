319465559a09d5e64ed261fe593336ce9bb5aea8
cellist <cellist@squizzy.de>
2021-03-04 14:51:58 +0100

    Threaded mandelbrot rendering

---
+++ qt/mandelbrot/go.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+rm -rf build TAGS
+etags *.cpp
+mkdir -p build &&\
+    cd build &&\
+    env CXX=/usr/bin/clang++ cmake .. &&\
+    cmake --build . &&\
+    cd ..
+
+[ -f ./build/mandelbrot ] && ./build/mandelbrot
+
