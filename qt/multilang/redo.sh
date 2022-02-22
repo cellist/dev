a27c1dbc744647c6e61af215b3c6c05312c2aed0
cellist <cellist@squizzy.de>
2022-02-22 12:07:46 +0100

    More steps, less resources

--- qt/multilang/redo.sh
+++ qt/multilang/redo.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
-rm -f Makefile  hellotr  hellotr_la.qm  main.o
+rm -f Makefile  hellotr  hellotr_la.qm  main.o hellotr_la.ts
+lupdate hellotr.pro 
+# simulate translation work in Qt Linguist through sed
+sed -i -e 's# type="unfinished">#>Orbis, te saluto!#' hellotr_la.ts
 lrelease hellotr_la.ts
 qmake
 make
