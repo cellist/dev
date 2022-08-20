b55f45d761ddd048b84d609095d03445868780f9
cellist <cellist@squizzy.de>
2022-08-20 19:04:56 +0200

    profile is more interesting now

--- profiling/profiling.cxx
+++ profiling/profiling.cxx
@@ -1,34 +1,51 @@
-// $ g++ -pg -o profiling profiling.cxx
+// $ g++ -pg -pedantic -Wall -o profiling profiling.cxx
 // $ ./profiling
 // $ gprof profiling gmon.out | less
 //
 #include <iostream>
 
+unsigned int compute1(const unsigned int x);
+unsigned int compute2(const unsigned int x);
+unsigned int compute3(const unsigned int x);
+unsigned int compute5(const unsigned int x);
+unsigned int compute7(const unsigned int x);
+unsigned int compute11(const unsigned int x);
+
 unsigned int compute1(const unsigned int x) {
- return 100-x;
+  
+  return 400-x;
 }
 
 unsigned int compute2(const unsigned int x) {
- return 2*x-1;
+
+  return (x%3) ? 2*x-1 : compute3(x+1);
 }
 
 unsigned int compute3(const unsigned int x) {
- return 2*x-2;
+  
+  return (x%5) ? 2*x-2 : compute5(x+1);
 }
 
 unsigned int compute5(const unsigned int x) {
- return 2*x-3;
+
+  return (x%7) ? 2*x-3 : compute7(x+1);
 }
 
 unsigned int compute7(const unsigned int x) {
- return 2*x-4;
+
+  return (x%11) ? 2*x-4 : compute11(x+1);
+}
+
+unsigned int compute11(const unsigned int x) {
+
+  return 2*x-5;
 }
 
 unsigned int newSeed(const unsigned int x) {
   
   unsigned int answer = 0;
 
-  if(x > 50) {
+  if(x > 200) {
     answer = compute1(x);
   } else if(!(x%2)) {
     answer = compute2(x);
@@ -46,7 +63,7 @@ unsigned int newSeed(const unsigned int x) {
 int main() {
   unsigned int seed = 42;
   
-  for(unsigned int i = 0; i < 20; i++) {
+  for(unsigned int i = 0; i < 100; i++) {
     std::cout << i << ": " << seed << std::endl;
     
     seed = newSeed(seed);
