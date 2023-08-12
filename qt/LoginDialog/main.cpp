ec6b75ea25a8518b586cece7cc8dbf145e3d3f08
cellist <cellist@squizzy.de>
2023-08-12 16:07:40 +0200

    added messagebox to show credentials

--- qt/LoginDialog/main.cpp
+++ qt/LoginDialog/main.cpp
@@ -1,18 +1,29 @@
 #include <QApplication>
+#include <QMessageBox>
+#include <QFont>
 #include "login_dialog.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
+    // Set the serif font
+    QFont serifFont("Times", 12, QFont::Normal);
+
     LoginDialog loginDialog;
     if (loginDialog.exec() == QDialog::Accepted)
     {
         Credentials credentials = loginDialog.getCredentials();
-        // You can use the entered credentials here
         QString username = credentials.getUsername();
         QString password = credentials.getPassword();
-        // Perform login validation or other actions with the credentials
+
+        // Display the entered username and password using a QMessageBox
+        QMessageBox msgBox;
+        msgBox.setWindowTitle("Entered Credentials");
+        msgBox.setText("Username: " + username);
+        msgBox.setDetailedText("Password: " + password);
+        msgBox.setFont(serifFont); // Set the serif font
+        msgBox.exec();
     }
 
     return 0;
